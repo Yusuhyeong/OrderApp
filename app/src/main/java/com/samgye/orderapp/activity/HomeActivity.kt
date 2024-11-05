a20aa5fc1cee5c37d79312dd035da010b572e6c6
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-11-05 17:30:18 +0900

    menu 애니메이션 효과 추가 (추후 변경)

--- app/src/main/java/com/samgye/orderapp/activity/HomeActivity.kt
+++ app/src/main/java/com/samgye/orderapp/activity/HomeActivity.kt
@@ -2,11 +2,61 @@ package com.samgye.orderapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.lifecycle.ViewModelProvider
 import com.samgye.orderapp.R
+import com.samgye.orderapp.activity.viewmodel.HomeViewModel
+import com.samgye.orderapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private var isMenuVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        binding.homeViewModel = viewModel
+        binding.lifecycleOwner = this
+
+//        viewModel.isMenuVisible.observe(this) { isVisible ->
+//            if (isVisible) {
+//                binding.clMenu.startAnimation(AnimationUtils.loadAnimation(this, R.anim.menu_slide_in))
+//                binding.clMenu.visibility = View.VISIBLE
+//            } else {
+//                binding.clMenu.startAnimation(AnimationUtils.loadAnimation(this, R.anim.menu_slide_out))
+//                binding.clMenu.visibility = View.GONE
+//            }
+//        }
+
+        viewModel.selected_id.observe(this) { id ->
+            Log.d("HomeActivity", "onClick : ${id.toString()}")
+            when(id) {
+                R.id.cl_dummy_menu.toString() -> {
+                    if (isMenuVisible) {
+                        binding.clMenu.startAnimation(AnimationUtils.loadAnimation(this, R.anim.menu_slide_out))
+                        binding.clMenu.visibility = View.GONE
+                        binding.clDummyMenu.visibility = View.GONE
+                        isMenuVisible = false
+                    }
+                }
+
+                R.id.iv_menu.toString() -> {
+                    if (!isMenuVisible) {
+                        binding.clMenu.startAnimation(AnimationUtils.loadAnimation(this, R.anim.menu_slide_in))
+                        binding.clMenu.visibility = View.VISIBLE
+                        binding.clDummyMenu.visibility = View.VISIBLE
+                        isMenuVisible = true
+                    }
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/samgye/orderapp/activity/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/samgye/orderapp/activity/viewmodel/HomeViewModel.kt
@@ -1,4 +1,22 @@
 package com.samgye.orderapp.activity.viewmodel
 
-class HomeViewModel {
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    private val _selected_id = MutableLiveData<String>()
+    val selected_id: LiveData<String>
+        get() = _selected_id
+    private val _isMenuVisible = MutableLiveData<Boolean>(false)
+    val isMenuVisible: LiveData<Boolean>
+        get() = _isMenuVisible
+    fun menuClick(view: View) {
+        _selected_id.value = view.id.toString()
+    }
+    private fun menuVisible() {
+        _isMenuVisible.value = !(_isMenuVisible.value ?: false)
+    }
 }
