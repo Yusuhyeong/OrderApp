c9a45f3ff36cfe4bcc688bd43a8932c8da9831cd
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-11-05 14:02:48 +0900

    정보 조회 api 구현 (서버 미완성)

--- app/src/main/java/com/samgye/orderapp/api/ApiClient.kt
+++ app/src/main/java/com/samgye/orderapp/api/ApiClient.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.samgye.orderapp.api.network.ApiFactory
 import com.samgye.orderapp.api.request.LoginRequest
 import com.samgye.orderapp.api.response.TokenResponse
+import com.samgye.orderapp.api.response.UserInfoResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,6 +20,10 @@ class ApiClient (
         callback: (token: TokenResponse?, error: Throwable?) -> Unit
     ) = manager.login(LoginRequest(snsId = id, snsType = type), callback)
 
+    fun hasToken(): Boolean {
+        return manager.hasToken()
+    }
+
     /**
      * 기존 토큰을 갱신합니다
      *
@@ -30,6 +35,37 @@ class ApiClient (
         oldToken: TokenResponse = tokenManagerProvider.manager.getToken() ?: throw Error("RefreshToken을 찾을 수 없습니다. 로그인해주세요."),
         callback: (token: TokenResponse?, error: Throwable?) -> Unit
     ) = manager.refreshAccessToken(oldToken, callback)
+
+
+    /**
+     * 사용자 정보를 조회합니다
+     *
+     * @param callback 보유 token에 대한 [UserInfoResponse] 반환.
+     */
+    fun userInfo(callback: (info: UserInfoResponse?, error: Throwable?) -> Unit) {
+        apiBearer.getUserInfo().enqueue(object : Callback<UserInfoResponse> {
+            override fun onResponse(
+                call: Call<UserInfoResponse>,
+                response: Response<UserInfoResponse>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.let { info ->
+                        Log.d("UserInfoResponse", "snsType: ${info.snsType}")
+                        Log.d("UserInfoResponse", "username: ${info.username}")
+                        return
+                    }
+                    callback(null, Throwable("응답오류. No Body"))
+                } else {
+                    callback(null, Throwable(response.errorBody().toString()))
+                }
+            }
+            override fun onFailure(
+                call: Call<UserInfoResponse>,
+                t: Throwable) {
+                callback(null, t)
+            }
+        })
+    }
 
     companion object {
         /**
